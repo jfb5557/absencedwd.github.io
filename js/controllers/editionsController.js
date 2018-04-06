f17792e0c3eed3b42aabf468c94bedcf6d40f4c3
fernandoleira <LeiraFernandoCortel@gmail.com>
2018-04-06 10:32:23 -0400

    Editions Page

---
+++ js/controllers/editionsController.js
@@ -0,0 +1,17 @@
+app.controller("editionsController", ['$scope', function($scope) {
+
+  $scope.editions =
+  [
+    {
+      year: 2016,
+      cover: 'img/edition-covers/cover2016.jpg',
+      pdf: ''
+    },
+    {
+      year: 2015,
+      cover: 'img/edition-covers/cover2016.jpg',
+      pdf: ''
+    },
+  ]
+
+}]);
